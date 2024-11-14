a77bbd8635c9dfe9f671e89c066933a8dd07fa92
nhennoejo <taishijinye@gmail.com>
2024-11-14 19:53:29 +0900

    frontmatterの画像が表示されないエラーを修正（画像urlが上手に解釈されてなかった）

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,15 @@ import { z, defineCollection } from 'astro:content';
 
 const blogCollection = defineCollection({
   type: 'content',
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
     author: z.string(),
     thumbnail: z.object({
-      url: z.string(),
+      url: image().refine((img) => img.width >= 1080, {
+        message: "カバー画像は幅1080ピクセル以上でなければなりません！",
+      }),
       alt: z.string(),
     }),
     tags: z.array(z.string()),
